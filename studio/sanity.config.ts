ffbd332c2a4cea40cdc3a8be52e7f6847baaac96
Shilo954 <shilotech@protonmail.com>
2024-08-23 19:03:03 +0400

    Update sanity.config.ts

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -17,16 +17,7 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
-    // Optional configuration
-
-    // Filter icons by collection for all Icon fields (this field has typed autocomplete ✨)
-    // Defaults to empty array (all collections)
-    collections: ['fa-brands', 'mdi', ...],
-
-    // Shows the selected icon name and collection underneath the icon picker
-    // Defaults to false
-    showName: false,
-
+    
 })
 
 
